68e6dacbf827740e264ea1ada7c74be801ce5e0c
Jiayuan Zhang <forrestchang7@gmail.com>
2026-07-10 20:30:40 +0800

    refactor(projects): enforce single-space ownership

--- packages/views/issues/components/issue-detail.tsx
+++ packages/views/issues/components/issue-detail.tsx
@@ -1551,6 +1551,7 @@ function IssueDetailInner({ issueId, onDelete, onDone, defaultSidebarOpen = true
             <ProjectPicker
               projectId={issue.project_id}
               onUpdate={handleUpdateField}
+              spaceId={issue.space_id}
             />
           </PropRow>
 
